04e0c2b0c29514211c5b1f8da3f857957d8f681b
Facundo Monteagudo <facundo_monteagudo@outlook.es>
2021-09-16 00:12:35 -0300

    basic home and tweet

---
+++ frontend/components/Tweet.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/Tweet.module.scss";
+
+interface Props {}
+
+const Login: React.FC<Props> = (props) => {
+  return <div></div>;
+};
+
+export default Login;
---
+++ frontend/pages/home.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import styles from "../styles/Home.module.scss";
+
+const Register: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}></div>
+      <div className={styles.content}>aaaaaaa</div>
+      <div className={styles.navbar}></div>
+    </div>
+  );
+};
+
+export default Register;
